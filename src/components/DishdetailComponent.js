2bddbf3df44ae2d3640add13b674ff1fedea05ec
Pralesh Rajbhandari <kiyubikid121@gmail.com>
2020-09-23 10:34:46 +0545

    single page application 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle , Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link } from 'react-router-dom';
     function RenderComments({comments}){
         const coments = comments.map(comment =>{
             return(
@@ -48,9 +49,19 @@ import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
             return (<div></div>)
         }
         const dishId = <RenderDish dish={props.dish}/>
-        const commentDish = <RenderComments comments={props.dish.comments}/>
+        const commentDish = <RenderComments comments={props.comments}/>
         return(
           <div className="container">
+              <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                                <h3>{props.dish.name}</h3>
+                                <hr/>
+                        </div>
+                 </div>
             <div className='row'>
                 {dishId}
                 {commentDish}
